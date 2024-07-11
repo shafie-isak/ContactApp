625cf155d82a3d090973592eff2ab49f19bdfeb7
shafie-isak <shafieisak@gmail.com>
2024-07-11 20:16:50 +0300

    Responsive

--- src/Components/Contact-details.jsx
+++ src/Components/Contact-details.jsx
@@ -4,17 +4,24 @@ import '../CssModule/ContactDetails.css';
 const ContactsDetails = ({ selectedContact, updateContact }) => {
   const [formState, setFormState] = useState({ ...selectedContact });
   const [imageFile, setImageFile] = useState(null);
+  const [showArrow, setShowArrow] = useState(false);
 
   useEffect(() => {
     setFormState({ ...selectedContact });
+    if (selectedContact) {
+      setShowArrow(true);
+    }
   }, [selectedContact]);
 
-  const handleInputChange = async (e) => {
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
     const updatedContact = { ...formState, [name]: value };
     setFormState(updatedContact);
+  };
+
+  const handleInputBlur = async () => {
     try {
-      await updateContact(updatedContact);
+      await updateContact(formState);
     } catch (error) {
       alert(`Error updating contact: ${error.message}`);
     }
@@ -43,27 +50,28 @@ const ContactsDetails = ({ selectedContact, updateContact }) => {
   }
 
   return (
-    <div className="Contacts-details-Container">
+    <div className="Contacts-details-Container" id={showArrow ? "showArrow" : ""}>
       <div className="Contacts-details">
+        <i className="fa-solid fa-arrow-right back" onClick={() => setShowArrow(!showArrow)}></i>
         <div className="Contact-details-header">
           <img src={formState.img} alt="" />
           <label htmlFor="Upload-img"><i className="fa-solid fa-camera-rotate"></i></label>
-          <input type="file" accept='image/*' id='Upload-img' onChange={handleImageChange} />
-          <input type='text' name="Name" value={formState.Name} onChange={handleInputChange} />
+          <input type="file" accept="image/*" id="Upload-img" onChange={handleImageChange} />
+          <input type="text" name="Name" value={formState.Name} onChange={handleInputChange} onBlur={handleInputBlur} />
         </div>
         <hr />
         <div className="Contact-Additional-Details">
           <div>
             <label htmlFor="Phone"><i className="fa-solid fa-phone"></i> Phone: </label>
-            <input type='text' name="Phone" value={formState.Phone} onChange={handleInputChange} />
+            <input type="text" name="Phone" value={formState.Phone} onChange={handleInputChange} onBlur={handleInputBlur} />
           </div>
           <div>
             <label htmlFor="Email"><i className="fa-solid fa-envelope"></i> Email: </label>
-            <input type='text' name="Email" value={formState.Email || ""} onChange={handleInputChange} />
+            <input type="text" name="Email" value={formState.Email || ""} onChange={handleInputChange} onBlur={handleInputBlur} />
           </div>
           <div>
             <label htmlFor="Address"><i className="fa-solid fa-location-dot"></i> Address: </label>
-            <input type='text' name="Address" value={formState.Address || ""} onChange={handleInputChange} />
+            <input type="text" name="Address" value={formState.Address || ""} onChange={handleInputChange} onBlur={handleInputBlur} />
           </div>
         </div>
       </div>
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import '../CssModule/Navigation.css';
+import '../App.css';
 import { NavLink } from "react-router-dom";
 
 // Navigation component for sidebar navigation
 const Navigation = ({ showNav, contactCount, handleshowNav }) => {
-  // Determine display style based on window width
-  const displayStyle = (window.innerWidth >= 601 && window.innerWidth <= 1024) ? (showNav ? "block" : "none") : "block";
   
   return (
-    <div className="Navigation" style={{ display: displayStyle }} >
-      <i className="fa-solid fa-xmark btn-Cancel" onClick={handleshowNav}></i>
+    <div className="Navigation" id={showNav ? "Hidden" : ""} >
+      <i class="fa-solid fa-xmark btn-Cancel" onClick={handleshowNav}></i>
       <nav>
         <NavLink to="/" className="Link hero-link">
           <i className="fa-solid fa-user"></i> My Contacts 
